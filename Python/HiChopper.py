be2edf34bd544a10efcc46b0f2b3407ee8e0afba
rekil2g <kevinlinux2020@gmail.com>
2023-05-02 08:11:56 -0300

    Primeiro codigo

--- Python/HiChopper.py
+++ Python/HiChopper.py
@@ -1,3 +1,23 @@
 # Programa HiChopper para Terminal Linux.
 # @author: brkevlinux
 #----------------------------------------
+
+def chopper():
+	print(
+		'    _____'
+		"\n \_|     |_/ ","-" * len(say),
+		"\n  _|  X  |_  '{}'".format(say),
+		"\n  |_______| /","-"*len(say),
+		"\n    (uO O) /"
+		"\n    / -º',"
+		"\n   (|___| )"
+		"\n    |___|"
+		"\n     | | "
+		"\n     ^ ^ "
+		)
+say = input()
+if say == 'hello':
+	say = 'Oi sou Tony Tony chopper! E esse é o HiChopper.'
+	chopper()
+else:
+	chopper()
