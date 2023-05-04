da82477d51c9c63fc8d7f394c1165613c2c2cc43
rekil2g <kevinlinux2020@gmail.com>
2023-05-04 08:11:46 -0300

    adicionando desenho em Java

---
+++ java/src/Hichopper.java
@@ -0,0 +1,20 @@
+
+public class Hichopper {
+
+	public static void main(String[] args) {
+		// HiChopper escrito em Java.
+		System.out.println(" ");
+		System.out.println("  ______");
+		System.out.println("\\_|     |_/");
+		System.out.println(" _|  X  |_");
+		System.out.println("|_________|");
+		System.out.println("  (uO O)");
+		System.out.println("  / -º',");
+		System.out.println(" (|___| )");
+		System.out.println("  |___|");
+		System.out.println("   | |");
+		System.out.println("   ^ ^ ");
+
+	}
+
+}
